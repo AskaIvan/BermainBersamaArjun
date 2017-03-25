669240ffa11f885eb5299e60fe91718e41ebf864
AskaIvan <aska_gunawan_24rpl@student.smktelkom-mlg.sch.id>
2017-03-25 22:01:54 +0700

    Duh lagek pertama

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl301040633/bermainbersamaarjun/MainActivity.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.project2.xirpl301040633.bermainbersamaarjun;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import at.markushi.ui.CircleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    CircleButton btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (CircleButton) findViewById(R.id.circlebutton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+
+            }
+        });
+    }
+}
